f604bc0785e04ad84f05bfca7aedf3743ccdfabb
Kyle Stokes <kst0k3@gmail.com>
2018-06-07 20:33:32 -0400

    Add Model, View, Controller groups

--- VirtualTourist/Controller/LocationsController.swift
+++ VirtualTourist/Controller/LocationsController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LocationsController.swift
 //  VirtualTourist
 //
 //  Created by Kyle Stokes on 6/7/18.
@@ -7,17 +7,15 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class LocationsController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
